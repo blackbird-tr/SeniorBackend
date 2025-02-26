785f349adbddc08738bbf791d55d83cc329475be
Evren KARAKUŞ <109800322+blackbird-tr@users.noreply.github.com>
2025-02-26 09:01:41 +0300

    dtos Created

---
+++ SeniorBackend/SeniorBackend/SeniorBackend.Core/DTOs/Account/AuthenticationResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorBackend.Core.DTOs.Account
+{
+    public class AuthenticationResponse
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; } 
+        public bool IsVerified { get; set; }
+        public string JWToken { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
