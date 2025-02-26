96f2d53970cf731b869aa64351ab4a3c166d23fe
Evren KARAKUŞ <109800322+blackbird-tr@users.noreply.github.com>
2025-02-26 09:02:34 +0300

    auth

---
+++ SeniorBackend/SeniorBackend/SeniorBackend.Core/Interfaces/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using SeniorBackend.Core.DTOs.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorBackend.Core.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<RegisterResponse> RegisterAsync(RegisterRequest request);
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+            
+        Task<ValidateRefreshTokenResponse> ValidateRefreshToken(string userId, string token);
+
+        Task<ExchangeRefreshTokenResponse> ExchangeRefreshToken(RequestRefreshToken requestRefreshToken);
+    }
+}
