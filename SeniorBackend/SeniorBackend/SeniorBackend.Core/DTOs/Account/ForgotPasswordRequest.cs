a9c775fc9a5f5ce16284de54b7201321dad7783e
Evren KARAKUŞ <109800322+blackbird-tr@users.noreply.github.com>
2025-02-28 18:04:46 +0300

    dto created

--- SeniorBackend/SeniorBackend/SeniorBackend.Core/DTOs/Account/ForgotPasswordRequest.cs
+++ SeniorBackend/SeniorBackend/SeniorBackend.Core/DTOs/Account/ForgotPasswordRequest.cs
@@ -10,5 +10,16 @@ namespace SeniorBackend.Core.DTOs.Account
     public class ForgotPasswordRequest
     {
         [Required][EmailAddress] public string Email { get; set; }
+        [Required] public string Token { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
