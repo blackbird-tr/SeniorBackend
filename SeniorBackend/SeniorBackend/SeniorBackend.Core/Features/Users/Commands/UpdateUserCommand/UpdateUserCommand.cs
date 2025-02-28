e9f5efa482d5fb5083bec931a0390b2e3963224c
Evren KARAKUŞ <109800322+blackbird-tr@users.noreply.github.com>
2025-02-28 21:27:15 +0300

    fixed update user

---
+++ SeniorBackend/SeniorBackend/SeniorBackend.Core/Features/Users/Commands/UpdateUserCommand/UpdateUserCommand.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using SeniorBackend.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorBackend.Core.Features.Users.Commands.UpdateUserCommand
+{
+    public class UpdateUserCommand:IRequest<string>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNum { get; set; }
+    }
+
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, string>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+           
+        }
+        public async Task<string> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user=await _userRepository.GetByIdAsync(request.Id);
+            if (user == null) { throw new Exception("user cannot find "); }
+
+            user.FirstName = request.Name;
+            user.LastName = request.Surname;
+            user.PhoneNumber = request.PhoneNum;
+            try
+            {
+                await _userRepository.UpdateAsync(user);
+                return user.Id;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+           
+        }
+    }
+}
